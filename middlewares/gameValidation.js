3bb22a7d83371ed4cbb56d4522b29fb8f69755d5
CaioPedras <caio.vp@gmail.com>
2022-07-24 18:24:07 -0300

    fix game post

--- middlewares/gameValidation.js
+++ middlewares/gameValidation.js
@@ -1,6 +1,7 @@
 import gameSchema from "../schemas/gameSchema.js";
 export default function validateGame(req, res, next) {
   const game = req.body;
+  console.log(game);
   const validation = gameSchema.validate(game);
   if (validation.error) {
     return res.sendStatus(400);
--- schemas/gameSchema.js
+++ schemas/gameSchema.js
@@ -1,8 +1,8 @@
 import joi from "joi";
 const gameSchema = joi.object({
   name: joi.string().required(),
-  image: joi.string().uri().required(),
-  sotckTotal: joi.string().required(),
+  image: joi.string().required().uri(),
+  stockTotal: joi.string().required(),
   categoryId: joi.number().required(),
   pricePerDay: joi.string().required(),
 });
