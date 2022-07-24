cc0b8dd6014ce453297987b95bbfdd5d3ed50afc
CaioPedras <caio.vp@gmail.com>
2022-07-24 13:51:10 -0300

    Game Controllers test

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -23,7 +23,28 @@ export async function getGames(req, res) {
   }
 }
 export async function createGame(req, res) {
+  const game = req.body;
   try {
+    const result = await db.query("SELECT id FROM categories WHERE id = $1", [
+      game.categoryId,
+    ]);
+    if (result.rows.length === 0) {
+      return res.sendStatus(400);
+    }
+    await db.query(
+      `
+    INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay")
+    VALUES($1,$2,$3,$4,$5)
+    `,
+      [
+        game.name,
+        game.image,
+        Number(game.stockTotal),
+        game.categoryId,
+        Number(game.pricePerDay),
+      ]
+    );
+    res.sendStatus(201);
   } catch (err) {
     console.log(err);
     res.sendStatus(500);
