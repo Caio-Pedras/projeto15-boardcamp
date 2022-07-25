f3f2d0e3c7f5b02bdc45f15d9408b1171c0b2bf0
CaioPedras <caio.vp@gmail.com>
2022-07-25 14:37:12 -0300

    deploy heroku

--- controllers/rentController.js
+++ controllers/rentController.js
@@ -61,16 +61,25 @@ export async function createRent(req, res) {
       return res.sendStatus(400);
     }
     const game = validGame.rows[0];
+    // const rentals = await db.query(
+    //   `SELECT * FROM rentals WHERE "gameId" = $1 "returnDate" is null`,
+    //   [game.id]
+    // );
+    // console.log(rentals.rows);
+    // if (game.stockTotal <= rentals.rows.length) {
+    //   return res.sendStatus(400);
+    // }
+
     const result = await db.query(
       `
-    SELECT id
+    SELECT *
     FROM rentals
-    WHERE "gameId"= $1 AND "returnDate" is null
+    WHERE "gameId"= $1 AND "returnDate" IS null
     `,
-      [rental.gameID]
+      [rental.gameId]
     );
     if (result.rows.length > 0) {
-      if (game.stockTotal === result.rows.length) {
+      if (game.stockTotal <= result.rows.length) {
         return res.sendStatus(400);
       }
     }
